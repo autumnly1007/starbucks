e99b9c09ba8ff1b90e12f2e52b4bbaa946f20e4a
autumnly1007 <autumnly1007@gmail.com>
2022-03-26 21:05:04 +0900

    toggle btn

--- js/main.js
+++ js/main.js
@@ -79,3 +79,16 @@ new Swiper('.promotion .swiper', {
     nextEl: '.promotion .swiper-next',
   }
 });
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener("click", () => {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add("hide");
+  } else {
+    promotionEl.classList.remove("hide");
+  }
+});
